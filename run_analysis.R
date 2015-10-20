86d29173efa799d5cba358f0810a123cf1715949
Katrina <katrinawono@gmail.com>
2015-10-20 12:38:31 -0700

    update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,3 @@
-# /Users/katrina/Dropbox/clean/.RData
-
 # You should create one R script called run_analysis.R that does the following. 
 # 1. Merges the training and the test sets to create one data set.
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
